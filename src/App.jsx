be1f34240ca19002cd6b190ef0a13279b1257aa6
Lean Yi Fan <leanyifan@Leans-MacBook-Air.local>
2025-08-18 00:05:11 +0800

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { IoCodeSlash, IoSend } from "react-icons/io5";
 import { GoogleGenAI } from "@google/genai";
 import ReactMarkdown from 'react-markdown';
 import Bubble from "./Bubble";
+import { motion, AnimatePresence } from "framer-motion";
 
 
 const App = () => {
@@ -49,23 +50,49 @@ const App = () => {
     <>
     <div className =" w-screen min-h-screen overflow-x-hidden text-black font-inter">
       <div className="absolute inset-0 overflow-hidden -z-10">
+              <AnimatePresence>
+        {!isResponseScreen && (
+          <motion.div
+            key="circles"
+            initial={{ opacity: 0, scale: 0.5, y: 50 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.5, y: -50 }}
+            transition={{ type: "spring", stiffness: 40, damping: 10 }}
+            className="w-full h-full flex justify-center items-center"
+          >
       <Bubble color="rgb(254,173,216)" />
       <Bubble color="rgb(164,150,246)" />
       <Bubble color="rgb(197, 132, 237)" />
+                </motion.div>
+        )}
+      </AnimatePresence>
     </div>
       {
         isResponseScreen?
         <div className="middle flex flex-col h-[85vh] w-full">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.1, ease: "easeInOut" }} // smooth fade-in
+        >
           <div className="header px-[35px] h-[80px] w-[100vw] flex justify-between items-center mx-auto">
             <h2 className='text-[30px] font-borel pt-[25px] text-[#333333]'>AskMe</h2>
             <button id='newChatBtn' onClick={newChat} className='cursor-pointer text-[16px] text-[#333333]'>New chat</button>
           </div>
+          </motion.div>
 
           <div className="messages flex-1 overflow-y-auto w-[90vw] mb-[-30px] md:w-[70vw] mx-auto">
             {
               messages?.map((msg, index) =>{
                 return (
-                    <div key={index} className={msg.type}><ReactMarkdown>{msg.text}</ReactMarkdown></div>
+                    <motion.div
+                      key={index}
+                      initial={{ y: 20, opacity: 0 }}     
+                      animate={{ y: 0, opacity: 1 }}     
+                      exit={{ y: -20, opacity: 0 }}         
+                      transition={{ type: "spring", stiffness: 120, damping: 12 }} 
+                      className={msg.type}
+                    ><ReactMarkdown>{msg.text}</ReactMarkdown></motion.div>
                   )
               })
             }
@@ -73,6 +100,11 @@ const App = () => {
           </div>
         </div>
         :
+      <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.1, ease: "easeInOut" }} // smooth fade-in
+      >
       <div className="middle w-full h-[80vh] md:h-[70vh] flex flex-col items-center justify-center px-4 py-4">
         <h1 className="text-[60px] md:text-[70px] mb-8 font-borel text-[#333333]">AskMe</h1>
         <div className="w-full overflow-x-auto">
@@ -102,17 +134,22 @@ const App = () => {
         </div>
       </div>
       </div>
+      </motion.div>
       }
-      <div className={`bottom w-[100vw] flex flex-col justify-between items-center text-[#555555] text-[16px] ${isResponseScreen ? "h-[15vh]" : "h-[30vh] md:h-[30vh]"}`}>
+     <motion.div
+      animate={{ height: isResponseScreen ? "15vh" : "30vh" }}
+      transition={{ duration: 0.7, ease: "circOut" }}
+      className="fixed bottom-0 left-0 w-full flex flex-col justify-between items-center text-[#555555] text-[16px]"
+    >
         <div className={`inputBox flex items-center backdrop-blur-[80px] bg-black/10 h-[60px] rounded-[30px] w-[90vw] md:w-[70vw]`} style={{boxShadow: "0px 4px 30px 5px rgba(0,0,0,0.05)"}}>
-          <input value={message} onChange={(e)=>{setMessage(e.target.value)}} onKeyDown={(e) => {if (e.key === 'Enter') {hitRequest();}}} type="text" className='p-[10px] pl-[30px] placeholder-[#888888] bg-transparent flex-1 outline-none border-none' placeholder='Write your message here...'/>
+            <input value={message} onChange={(e)=>{setMessage(e.target.value)}} onKeyDown={(e) => {if (e.key === 'Enter') {hitRequest();}}} type="text" className='p-[10px] pl-[30px] placeholder-[#888888] bg-transparent flex-1 outline-none border-none' placeholder='Write your message here...'/>
           {
             message == "" ? "" : <i className='mr-5 cursor-pointer text-[22px] text-{#555555]' onClick={hitRequest}><IoSend/></i>
           }
         </div>
         <p className='mb-[20px] text-[14px] text-[#888888]'>Your very own personal assistant, powered by Gemini API.</p>
+        </motion.div>
       </div>
-    </div>
     </>
   )
 }
